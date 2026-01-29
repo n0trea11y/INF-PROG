61c2f8b77f41d604395add662621fbfbd6ec4357
n0trea11y <gierkistefanski@gmail.com>
2026-01-29 14:12:11 +0100

    nowe zadania na lekcji

---
+++ 1 Pierwszy program w C++/podstwa c++.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "witaj w programowaniu C++";
+
+	return 0;
+}
---
+++ 1 Pierwszy program w C++/zadanie 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int b;
+	int a;
+	cout << "podaj pierwsza liczbe = ";
+	cin >> a;
+	cout << "podaj druga liczbe = ";
+	cin >> b;
+	int c = a + b;
+	int d = a - b;
+	int e = a * b;
+	cout << "suma to";
+	cout << c;
+	cout << "roznica to";
+	cout << d;
+	cout << "iloczyn to";
+	cout << 3;
+	return 0;
+
+}
